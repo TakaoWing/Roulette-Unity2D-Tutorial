ecd1528a1a11581648ff6efb28785baea8b5c54a
TakaoWing <tt.wing2838783@gmail.com>
2017-04-05 23:24:46 +0900

    Complete Roullet Game

--- Assets/Scripts/rouletteController.cs
+++ Assets/Scripts/rouletteController.cs
@@ -6,6 +6,9 @@ public class rouletteController : MonoBehaviour {
 
 	float rotSpeed = 0; // 回転速度
 
+	private const float ROTSPEED = 100; // 回転速度
+	private const float SPEEDCOEF = 0.98f; // 減衰係数
+
 	void Start () {
 		
 	}
@@ -13,9 +16,12 @@ public class rouletteController : MonoBehaviour {
 	void Update () {
 		
 		if (Input.GetMouseButtonDown (0)) { // マウスが押されたら回転速度を設定する
-			this.rotSpeed = 10;
+			this.rotSpeed = ROTSPEED;
 		}
 
 		transform.Rotate (0, 0, this.rotSpeed); // 回転速度分、ルーレットを回転させる
+
+		this.rotSpeed *= SPEEDCOEF; // ルーレットを減速させる
+
 	}
 }
