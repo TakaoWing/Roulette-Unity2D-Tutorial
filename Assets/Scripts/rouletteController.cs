6121a3bc5f033b19b98cb0cfdd0d1e5cdc574687
TakaoWing <tt.wing2838783@gmail.com>
2017-04-05 23:07:42 +0900

    end chapter 3-5

---
+++ Assets/Scripts/rouletteController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rouletteController : MonoBehaviour {
+
+	float rotSpeed = 0; // 回転速度
+
+	void Start () {
+		
+	}
+
+	void Update () {
+		
+		if (Input.GetMouseButtonDown (0)) { // マウスが押されたら回転速度を設定する
+			this.rotSpeed = 10;
+		}
+
+		transform.Rotate (0, 0, this.rotSpeed); // 回転速度分、ルーレットを回転させる
+	}
+}
